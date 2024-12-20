30c907c7fe2b262b8ef8fe97981c5eb7be2aff1c
Daniel <daniil_telenkov@vk.com>
2024-12-20 20:49:30 +0300

    Добавьте файлы проекта.

---
+++ PlannerApi/Controllers/SystemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlannerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok($"[{nameof(SystemController)}] is already for job");
+        }
+    }
+}
